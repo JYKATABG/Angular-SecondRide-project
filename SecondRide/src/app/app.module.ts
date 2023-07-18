2e820264cd2f74481e163650ee655581e9137f86
Jykata <jykatabg@gmail.com>
2023-07-19 01:46:15 +0300

    Basic files structured
    Added and Configured: Routing,Templates + firebase

---
+++ SecondRide/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
+import { CoreModule } from './core/core.module';
+import { HomeComponent } from './home/home.component';
+import { UserModule } from './user/user.module';
+import { AboutComponent } from './layouts/about/about.component';
+import { ContactUsComponent } from './layouts/contact-us/contact-us.component';
+import { NotFoundComponent } from './layouts/not-found/not-found.component';
+import { OffersCatalogComponent } from './offers-catalog/offers-catalog.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactUsComponent,
+    NotFoundComponent,
+    OffersCatalogComponent,
+  ],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    UserModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ SecondRide/src/app/offers-catalog/offers-catalog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offers-catalog',
+  templateUrl: './offers-catalog.component.html',
+  styleUrls: ['./offers-catalog.component.css']
+})
+export class OffersCatalogComponent {
+
+}
