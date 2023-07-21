7e8063c530b4bef6a84264a592777931a001b7cd
Jykata <jykatabg@gmail.com>
2023-07-21 14:35:50 +0300

    completed register form + validations

--- SecondRide/src/app/user/register/register.component.ts
+++ SecondRide/src/app/user/register/register.component.ts
@@ -7,7 +7,12 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  register(form: NgForm) {
-    console.log(form.value);
+  register(form: NgForm): void {
+    if (form.invalid) {
+      return alert('The form is invalid');
+    }
+
+    const value: { email: string; password: string } = form.value;
+    console.log(value);
   }
 }
